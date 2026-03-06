8dbebe3d3a47f895baf77ba904d720936cd7839f
Lubhana <141313752+Lubhana@users.noreply.github.com>
2026-03-06 16:12:20 +0530

    Implement contrastive loss function
    Added a contrastive loss function for model training.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ import tensorflow as tf
 import cv2
 from PIL import Image
 
+
+def contrastive_loss(y_true, y_pred):
+    margin = 1.0
+    square_pred = tf.square(y_pred)
+    margin_square = tf.square(tf.maximum(margin - y_pred, 0))
+    return tf.reduce_mean(y_true * square_pred + (1 - y_true) * margin_square)
+
 model1 = tf.keras.models.load_model("models/model1.keras")
 model2 = tf.keras.models.load_model("models/model2.keras")
 
